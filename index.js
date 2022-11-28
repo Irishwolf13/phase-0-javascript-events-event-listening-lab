b4d7fa04d0e24ba719ac627dcad472df0dcdca5d
John Rooney <rooneyjohn@gmail.com>
2022-11-28 11:17:20 -0500

    commit tree 15546b66afabdf78a91892f3548978bafe529d61

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.getElementById("button");
+    function myAlert() {
+        alert("I was clicked");
+    }
+    input.addEventListener('click', myAlert);
 }
